ebd00059c81614631d354a7b83b0dc968c54563e
shivambhandari99 <shivambhandari99@gmail.com>
2022-03-27 11:17:48 -0500

    Checking dataset

--- sample_code/task1/task1_dataset.py
+++ sample_code/task1/task1_dataset.py
@@ -32,8 +32,8 @@ class RoadDataset(torch.utils.data.Dataset):
         
         self.imgs = [ img_name_prefix + json_name.split('_')[-1].split('.geojson')[0]+'.tif'
                      for json_name in self.jsons]
-        print(self.jsons)
-        print(self.imgs)
+        print(self.jsons[1000])
+        print(self.imgs[1000])
 
     
         
@@ -45,6 +45,7 @@ class RoadDataset(torch.utils.data.Dataset):
         # 2. Read image and calculate the mask 
         # 3. Apply transformations
         # 4. Return image and mask 
+
 
         img = 0
         mask = 0
