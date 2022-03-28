9172542455cfc91660ae52486e51c0655b1e4a3e
shivambhandari99 <shivambhandari99@gmail.com>
2022-03-27 23:48:52 -0500

    static function fix

--- sample_code/task1/task1_dataset.py
+++ sample_code/task1/task1_dataset.py
@@ -62,7 +62,7 @@ class RoadDataset(torch.utils.data.Dataset):
         img = img.astype(np.int16)
         img = self.transforms(img)
         complementary_mask = self.transforms(complementary_mask)
-        mask = np.stack(mask,complementary_mask)
+        mask = torch.stack([mask,complementary_mask])
         mask = self.transforms(mask)
         return img, mask
 
