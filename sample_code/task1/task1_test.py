bc718dc72782a236d2425fe5c068526762fe39a7
shivambhandari99 <shivambhandari99@gmail.com>
2022-03-28 19:49:58 -0500

    Test dry run

--- sample_code/task1/task1_test.py
+++ sample_code/task1/task1_test.py
@@ -63,6 +63,7 @@ def main(args):
         outputs = model(img.float())
 
         print(outputs)
+        print(outputs.shape)
         # TODO
         # 1. load test image
         # 2. convert test image into tensor
