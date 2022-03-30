0a1f42f789629d3ceef3edbc65497b63c7810fe8
shivambhandari99 <shivambhandari99@gmail.com>
2022-03-29 21:50:08 -0500

    Output numpy array

--- sample_code/task1/task1_test.py
+++ sample_code/task1/task1_test.py
@@ -91,7 +91,6 @@ def main(args):
         out_path = args.output_dir+'/'+img_path
         print(out_path)
         cv2.imwrite(out_path, out_img)
-        exit(1)
         
         
 if __name__ == '__main__':
