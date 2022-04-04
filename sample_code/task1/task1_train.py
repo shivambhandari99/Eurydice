f48186da5b3eae42ed369b35afb563192ace648f
shivambhandari99 <shivambhandari99@gmail.com>
2022-04-03 22:55:38 -0500

    UNet Code

--- sample_code/task1/task1_train.py
+++ sample_code/task1/task1_train.py
@@ -70,6 +70,7 @@ def main(args):
             inputs = inputs.float()
             optimizer.zero_grad()
             outputs = model(inputs)
+            print(outputs.shape)
             print(type(outputs))
             loss = criterion(outputs['out'], labels)
             running_loss += loss.item()
