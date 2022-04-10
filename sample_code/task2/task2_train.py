c67770bba74661a9bbb0a6cfd1f9865cfca7ffcf
shivambhandari99 <shivambhandari99@gmail.com>
2022-04-09 22:42:04 -0500

    changed LR for adam

--- sample_code/task2/task2_train.py
+++ sample_code/task2/task2_train.py
@@ -57,8 +57,8 @@ def main(mode='train'):
     
     
         
-    #params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.Adam(params, lr=0.0001)
     
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
                                                    step_size=3,
