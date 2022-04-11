e7d07faae2465bf40fbf5bb3a5cd31cc96b8df4c
shivambhandari99 <shivambhandari99@gmail.com>
2022-04-11 11:55:02 -0500

    ScaleJitter

--- sample_code/task2/task2_train.py
+++ sample_code/task2/task2_train.py
@@ -23,7 +23,7 @@ def get_transform(train):
     transforms = []
     transforms.append(T.ToTensor())
     if train:
-        transforms.append(T.RandomPhotometricDistort())
+        transforms.append(T.ScaleJitter())
     return T.Compose(transforms)
 
 
@@ -70,7 +70,7 @@ def main(mode='train'):
 
     for epoch in range(num_epochs):
         train_one_epoch(model, optimizer, data_loader_train, device, epoch, 1, scaler=None)
-        torch.save(model.state_dict(), os.path.join(model_save_dir, 'ep_aug3_' + str(epoch) +'.pth'))
+        torch.save(model.state_dict(), os.path.join(model_save_dir, 'ep_aug4_' + str(epoch) +'.pth'))
         print("--------------------")
         evaluate_loss(model, data_loader_val, device=device)
         evaluate(model, data_loader_val, device=device)
