a3e86b80a95f218f76c203cf8151266cc0f3ca47
shivambhandari99 <shivambhandari99@gmail.com>
2022-04-10 21:13:37 -0500

    New LR

--- sample_code/task2/task2_train.py
+++ sample_code/task2/task2_train.py
@@ -72,7 +72,7 @@ def main(mode='train'):
 
     for epoch in range(num_epochs):
         train_one_epoch(model, optimizer, data_loader_train, device, epoch, 1, scaler=None)
-        torch.save(model.state_dict(), os.path.join(model_save_dir, 'ep_rerun_' + str(epoch) +'.pth'))
+        torch.save(model.state_dict(), os.path.join(model_save_dir, 'ep_rerun2_' + str(epoch) +'.pth'))
         print("--------------------")
         evaluate_loss(model, data_loader_val, device=device)
         evaluate(model, data_loader_val, device=device)
